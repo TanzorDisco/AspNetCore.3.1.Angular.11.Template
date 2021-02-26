772aef60d1412f3c09c9acdd35ffc861fd894e69
TanzorDisco <sungurov.islam@gmail.com>
2021-02-26 22:13:18 +0300

    Left Navigation Panel

--- Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  styleUrls: ['./nav-menu.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NavMenuComponent {
   isExpanded = false;
